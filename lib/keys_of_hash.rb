c59e4685c9c7b3e7c6943ec2453959ddede7109b
Redmond Chan <redmondchan92@gmail.com>
2019-01-07 02:33:19 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,11 +1,11 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     array = []
     self.each do |key, value|
       if arguments.include?(value)
         array << key
       end
     end
-    array# code goes here
+    array
   end
 end
